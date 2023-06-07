84fd435e9525cef4689230697aec9766a0bec7b6
Boudjada Yasser <by.root96@gmail.com>
2023-06-07 17:53:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import sys
 import re
 import time
 from tabulate import tabulate
+#from PIL import ImageFont, ImageDraw
 
 def clone_repos(GITHUB_ACCESS_TOKEN,GITHUB_USERNAME):
     g = Github(GITHUB_ACCESS_TOKEN)
@@ -168,12 +169,121 @@ def format_table(data):
     # Sort the data by Total Lines in descending order
     sorted_data = sorted(data, key=lambda x: x['Total Lines'], reverse=True)
     table = []
+
     for d in sorted_data:
+        files = generate_svg_code(f"{d['Language']} Files", format_number(d['Files']), "#08C")
+        total_lines = generate_svg_code(f"{d['Language']} Total Lines", format_number(d['Total Lines']), "#08C")
+        code_lines = generate_svg_code(f"{d['Language']} Code Lines", format_number(d['Code Lines']), "#08C")
+        comment_lines = generate_svg_code(f"{d['Language']} Comment Lines", format_number(d['Comment Lines']), "#08C")
+        empty_lines = generate_svg_code(f"{d['Language']} Empty Lines", format_number(d['Empty Lines']), "#08C")
+
+        save_file(f"badges/{d['Language']}",f"{d['Language']}_files.svg", files)
+        save_file(f"badges/{d['Language']}",f"{d['Language']}_total_lines.svg", total_lines)
+        save_file(f"badges/{d['Language']}",f"{d['Language']}_code_lines.svg", code_lines)
+        save_file(f"badges/{d['Language']}",f"{d['Language']}_comment_lines.svg", comment_lines)
+        save_file(f"badges/{d['Language']}",f"{d['Language']}_empty_lines.svg", empty_lines)
+
         row = [d['Language'], d['Files'], d['Total Lines'], d['Code Lines'], d['Comment Lines'], d['Empty Lines']]
         table.append(row)
+    
+
+    total_files = generate_svg_code(f"Files in All GitHub Repos", format_number(sum(d['Files'] for d in data)), "#08C")
+    total_total_lines = generate_svg_code(f"Total Lines of Code (GitHub Repos)", format_number(sum(d['Total Lines'] for d in data)), "#08C")
+    total_code_lines = generate_svg_code(f"Code Lines (GitHub Repos)", format_number(sum(d['Code Lines'] for d in data)), "#08C")
+    total_comment_lines = generate_svg_code(f"Comment Lines (GitHub Repos)", format_number(sum(d['Comment Lines'] for d in data)), "#08C")
+    total_empty_lines = generate_svg_code(f"Empty Lines (GitHub Repos)", format_number(sum(d['Empty Lines'] for d in data)), "#08C")
+
+    save_file(f"badges",f"total_files.svg", total_files)
+    save_file(f"badges",f"total_lines.svg", total_total_lines)
+    save_file(f"badges",f"total_code_lines.svg", total_code_lines)
+    save_file(f"badges",f"total_comment_lines.svg", total_comment_lines)
+    save_file(f"badges",f"total_empty_lines.svg", total_empty_lines)
+
+
     table.append(['TOTAL', sum(d['Files'] for d in data), sum(d['Total Lines'] for d in data),
                   sum(d['Code Lines'] for d in data), sum(d['Comment Lines'] for d in data), sum(d['Empty Lines'] for d in data)])
     return tabulate(table, headers, tablefmt='pipe')
+
+def save_file(directory, filename, content):
+    os.makedirs(directory, exist_ok=True)
+    file_path = os.path.join(directory, filename)
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+def format_number(number):
+    suffixes = ['', 'k', 'm', 'b', 't']  # Define suffixes for each level of magnitude
+    magnitude = 0
+
+    # Determine the appropriate magnitude level
+    while abs(number) >= 1000:
+        magnitude += 1
+        number /= 1000
+
+    # Format the number with the appropriate suffix
+    formatted_number = f"{number:.1f}{suffixes[magnitude]}"
+
+    return formatted_number
+
+
+def generate_svg_code(new_text_1, new_text_2, new_color="#08C"):
+    svg_code = '''<svg width="79.5" height="20" viewBox="0 0 795 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="releases: 41">
+      <title>releases: 41</title>
+      <linearGradient id="a" x2="0" y2="100%">
+        <stop offset="0" stop-opacity=".1" stop-color="#EEE"/>
+        <stop offset="1" stop-opacity=".1"/>
+      </linearGradient>
+      <mask id="m"><rect width="795" height="200" rx="30" fill="#FFF"/></mask>
+      <g mask="url(#m)">
+        <rect width="555" height="200" fill="#555"/>
+        <rect width="240" height="200" fill="#08C" x="555"/>
+        <rect width="795" height="200" fill="url(#a)"/>
+      </g>
+      <g aria-hidden="true" fill="#fff" text-anchor="start" font-family="Verdana,DejaVu Sans,sans-serif" font-size="110">
+        <text x="60" y="148" textLength="455" fill="#000" opacity="0.25">releases</text>
+        <text x="50" y="138" textLength="455">releases</text>
+        <text x="610" y="148" textLength="140" fill="#000" opacity="0.25">41</text>
+        <text x="600" y="138" textLength="140">41</text>
+      </g>
+    </svg>'''
+
+    def calculate_text_length(text):
+        
+        font_size = 110
+        character_width = font_size * 0.6
+        ss=character_width * len(text)
+        return ss
+
+    text_width_1 = calculate_text_length(new_text_1)
+    text_width_2 = calculate_text_length(new_text_2)
+
+    svg_code = svg_code.replace('releases', new_text_1)
+    svg_code = svg_code.replace('41', new_text_2)
+
+    svg_code = svg_code.replace('textLength="455"', f'textLength="{text_width_1}"')
+    svg_code = svg_code.replace('textLength="140"', f'textLength="{text_width_2}"')
+
+    svg_code = svg_code.replace('width="555"', f'width="{text_width_1 + 100}"')
+    svg_code = svg_code.replace('width="240"', f'width="{text_width_2 + 100}"')
+
+    svg_code = svg_code.replace('width="795"', f'width="{(text_width_1 + text_width_2) + 200}"')
+
+    svg_code = svg_code.replace('width="79.5"', f'width="{((text_width_1 + text_width_2) + 200) / 10}"')
+
+    svg_code = svg_code.replace('0 0 795 200', f'0 0 {((text_width_1 + text_width_2) + 200)} 200')
+
+    svg_code = svg_code.replace('x="555"', f'x="{text_width_1 + 100}"')
+
+    svg_code = svg_code.replace('x="610"', f'x="{text_width_1 + 135}"')
+    svg_code = svg_code.replace('x="600"', f'x="{text_width_1 + 145}"')
+
+    svg_code = svg_code.replace('#08C', new_color)
+
+    return svg_code
+
+
+
+
+
 
 
 if __name__ == '__main__':
